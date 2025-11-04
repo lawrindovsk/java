38e6fdf941266a0c5283565fc493953e42edc8a8
Gustavo Laurindo dos Santos <104037685+lawrindovsk@users.noreply.github.com>
2025-11-03 22:44:39 -0300

    Add files via upload

--- java-introduction/src/MeuPrimeiroPrograma.java
+++ java-introduction/src/MeuPrimeiroPrograma.java
@@ -1,16 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
 // Arquivo: MeuPrimeiroPrograma.java
 public class MeuPrimeiroPrograma {
 
     public static void main(String[] args) {
+//        // [1] "new" cria o Objeto (a "coisa real")
+//        // Estamos chamando o Construtor da classe Cliente
+//        Cliente cliente1 = new Cliente("Ana Silva", "ana@email.com");
+//        Cliente cliente2 = new Cliente("Bruno Costa", "bruno@email.com");
+//        // [2] Usando os métodos
+//        System.out.println("Nome do cliente 1: " + cliente1.getNome()); // Usa o getNome()
+//        cliente2.desativar(); // Chama a ação de desativar
 
-        // [1] "new" cria o Objeto (a "coisa real")
-        // Estamos chamando o Construtor da classe Cliente
-        Cliente cliente1 = new Cliente("Ana Silva", "ana@email.com");
-        Cliente cliente2 = new Cliente("Bruno Costa", "bruno@email.com");
+        // 3 - lista para guardar usuiarios
+        // sintaxe parecida com c#
+        //new ArrayList<*tipo*>() = é como eu estaria dizendo, "crie uma nova lista em branco".
+        List<Cliente> meusClientes = new ArrayList<>();
 
-        // [2] Usando os métodos
-        System.out.println("Nome do cliente 1: " + cliente1.getNome()); // Usa o getNome()
+        //4 - criar e add clientes na lista;
+        meusClientes.add(new Cliente("Gustavo Laurindo", "gustavo.lsantos05@gmail.com"));
+        meusClientes.add(new Cliente("Elza de Jesus", "elza.jesus@gmail.com"));
+        meusClientes.add(new Cliente("Pyter Laurindo", "pyter.lauriindo@gmail.com"));
+        meusClientes.add(new Cliente("Julia Vitoria Maciel Figueiredo", "julia.figueiro@gmail.com"));
 
-        cliente2.desativar(); // Chama a ação de desativar
+        //5 - printar os clientes;
+        System.out.println("Total de clientes na list: " + meusClientes.size());
+
+        //6 - percorre na lista (em c# seria foreach)
+        // é praticamente igual
+        System.out.println("+++ Lista de Clientes");
+        for (Cliente cliente: meusClientes){
+            // para cada cliente DENTRO da lista meusClientes, printa
+            System.out.println("Nome: " + cliente.getNome());
+        }
+
+        //7 - pegar um item especifico (get num cliente de um indicie especifico, tipo o indice 0)
+        Cliente primeiroCliente = meusClientes.get(0);
+        System.out.println("O primeiro cliente da lista é: " + primeiroCliente.getNome());
+
     }
+
+
 }
